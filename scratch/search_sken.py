c4943b7b297ba80ed13abd96ea773ab8c944c404
ezra <ezranewbie17@gmail.com>
2026-06-12 21:30:05 +0700

    up

--- scratch/search_sken.py
+++
@@ -1,6 +0,0 @@
-with open("c:/Users/USER/Desktop/System Administator/Private-Cloud/README.md", "r", encoding="utf-8") as f:
-    content = f.read()
-
-import re
-matches = re.findall(r"(?i)sken\w*", content)
-print("Matches for 'sken*':", set(matches))
